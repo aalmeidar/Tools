e5afdd00b00ae3210f53c69aea3ffbb2d957d362
Almeida <72100006+aalmeidar@users.noreply.github.com>
2022-05-11 19:00:56 +0000

    Delete arp_scan_passive.py

--- arpScan/arp_scan_passive.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import *
-
-def scan(pkt):
-	if ARP in pkt and pkt[ARP].op == 2:
-		return pkt.sprintf("[+] MAC: %ARP.hwsrc% IP: %ARP.psrc%")
-		
-sniff(prn=scan, filter="arp", store = 0)
